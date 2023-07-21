8a111287971fc2757ed2a192509f3aeb61dbb972
ardududududuinoman <shalompmc0505@naver.com>
2023-07-21 23:45:55 +0900

    가족여행 일정끝 ts공부시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import "normalize.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { styled } from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Container></Container>
+      {/* <Navbar></Navbar> */}
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  bottom: 0;
+  background-color: skyblue;
+  display: flex;
+`;
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,13 @@
 import { styled } from "styled-components";
 
 const Navbar = () => {
-  return (
-    <>
-      <Container></Container>
-    </>
-  );
+  return <Container>dd</Container>;
 };
 const Container = styled.div`
   width: 100px;
   height: 100px;
+  position: absolute;
+  bottom: 0;
   background-color: skyblue;
 `;
 export default Navbar;
