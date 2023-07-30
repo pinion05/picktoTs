a528c258bb698fcd1a2710616304e894ff5c7464
ardududududuinoman <shalompmc0505@naver.com>
2023-07-30 20:19:41 +0900

    PropsErrorFix

--- src/components/ArrayContainer.tsx
+++ src/components/ArrayContainer.tsx
@@ -3,26 +3,35 @@ import img1 from "../img/8machine-_-mTnn1HYD_KU-unsplash.jpg";
 import img2 from "../img/abik-peravan-doW8Spg4Zy8-unsplash.jpg";
 import img3 from "../img/cash-macanaya-u24e_r6BsRE-unsplash.jpg";
 import img4 from "../img/david-emrich-X1Hozg__MiA-unsplash.jpg";
-const ArrayContainer: React.FC = () => {
+import { styled } from "styled-components";
+
+interface ArrayContainerProps {
+  column: number;
+  imgArray: string[];
+}
+const ArrayContainer: React.FC<ArrayContainerProps> = ({
+  column,
+  imgArray,
+}) => {
+  //
   function shuffle(array: Array<any>) {
     return array.sort(() => Math.random() - 0.5);
   }
-  const imgArray = [img1, img2, img3, img4];
+  // const imgArray = [img1, img2, img3, img4];
 
-  function lngth100ImgArray(): Array<any>[] {
+  function lngthTimeImgArray(a: number): Array<any> {
     let beforMixImgArray: any = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < a; i++) {
       beforMixImgArray = beforMixImgArray.concat(imgArray);
     }
     return shuffle(beforMixImgArray);
   }
-  const arrayA: any[] = lngth100ImgArray();
 
   const array1: string[] = [];
   const array2: string[] = [];
   const array3: string[] = [];
 
-  arrayA.forEach((image, index) => {
+  lngthTimeImgArray(1).forEach((image, index) => {
     if (index % 3 === 0) {
       array1.push(image);
     } else if (index % 3 === 1) {
@@ -34,11 +43,20 @@ const ArrayContainer: React.FC = () => {
 
   return (
     <>
-      <PostArray data={array1} />
-      <PostArray data={array2} />
-      <PostArray data={array3} />
+      <Container>
+        <PostArray data={array1} />
+        <PostArray data={array2} />
+        <PostArray data={array3} />
+      </Container>
     </>
   );
 };
 
 export default ArrayContainer;
+
+const Container = styled.div`
+  width: auto;
+  height: 30000px;
+  background-color: yellow;
+  display: flex;
+`;
