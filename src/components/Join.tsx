56be04767bf23423ce8b0dbadfe70f5aca8ff67b
ardududududuinoman <shalompmc0505@naver.com>
2023-08-12 01:41:58 +0900

    axiosPost

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -2,57 +2,99 @@ import React from "react";
 import { useState } from "react";
 import { styled } from "styled-components";
 import { css as stCss } from "styled-components";
-import { Button } from "@mui/material";
+import { Button, Input } from "@mui/material";
 import { css } from "@emotion/react";
+import { green } from "@mui/material/colors";
+import axios from "axios";
 
 const Join = () => {
   //
-  const [nickName, setNickName] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordCheck, setPasswordCheck] = useState<string>("");
+
   //
-
-  function inputOnChange(e: any, k: string) {
-    if (k === `email`) setEmail(e.target.value);
-    if (k === `pw`) setPassword(e.target.value);
-  }
-
-  function changeNikcname(e: React.ChangeEvent<HTMLInputElement>) {
-    setNickName(e.target.value);
-  }
-
-  function hadleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log("서밋");
-  }
 
   function isValidEmail(email: string) {
     const regex = /^[\w-_.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return regex.test(email);
   }
 
+  function hadleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    postRequest();
+  }
+
+  async function postRequest() {
+    try {
+      const data = {
+        nickname: nickname,
+        email: email,
+        password: password,
+      };
+      const resqonse = await axios.post(
+        "http://localhost:5000/api/register",
+        data
+      );
+      console.log(resqonse.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function changeInput(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    k: string
+  ) {
+    if (k === `nick`) setPasswordCheck(e.target.value);
+    if (k === `email`) setEmail(e.target.value);
+    if (k === `pw`) setPassword(e.target.value);
+    if (k === `pwc`) changePasswordCheck(e);
+  }
+
+  function changePasswordCheck(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    setPasswordCheck(e.target.value);
+    if (password === e.target.value) {
+      e.target.style.color = "#81c784";
+    } else {
+      e.target.style.color = "red";
+    }
+  }
+
   return (
     <>
-      <i className="fi fi-rr-eye"></i>
-      <Button />
       <Container onSubmit={(e) => hadleSubmit(e)}>
         <p className="marginbot">회원가입</p>
         <Text>닉네임</Text>
-        <Input type="text" placeholder="닉네임" />
+        <Input
+          type="text"
+          placeholder="닉네임을 입력하세요"
+          onChange={(e) => changeInput(e, "nick")}
+        />
         <Text>이메일</Text>
         <Input
           value={email}
-          placeholder="이메일"
-          onChange={(e) => inputOnChange(e, `email`)}
+          placeholder="abcd@gmail.com"
+          onChange={(e) => changeInput(e, `email`)}
         />
-        <Text>비밀번호</Text>
+        <Text>비밀번호 </Text>
         <Input
           value={password}
-          type="password"
+          type="text"
           placeholder="비밀번호"
-          onChange={(e) => inputOnChange(e, `pw`)}
+          onChange={(e) => changeInput(e, `pw`)}
+          style={{ marginBottom: "5px" }}
         />
-        <JoinButton type="submit">회원가입</JoinButton>
+        <Input
+          onChange={(e) => changeInput(e, "pwc")}
+          style={{ marginBottom: "5px" }}
+          placeholder="비밀번호 확인"
+          type="password"
+        />
+        <Button type="submit">회원가입</Button>
       </Container>
     </>
   );
@@ -79,12 +121,12 @@ const Container = styled.form`
   padding: 20px;
 `;
 
-const Input = styled.input`
-  height: 25px;
-  margin-bottom: 10px;
-  padding: 5px;
-  border-radius: 5px;
-`;
+// const Input = styled.input`
+//   height: 25px;
+//   margin-bottom: 10px;
+//   padding: 5px;
+//   border-radius: 5px;
+// `;
 
 const JoinButton = styled.button`
   width: 100%;
