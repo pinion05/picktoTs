e9075392a709a3cfd48232bd5b7495a428ae7968
ardududududuinoman <shalompmc0505@naver.com>
2023-08-24 11:10:48 +0900

    s3 get async

--- src/awsS3.ts
+++ src/awsS3.ts
@@ -1,5 +1,6 @@
 import AWS from "aws-sdk";
 import s3config from "./s3config.json";
+import { data } from "autoprefixer";
 
 const ACCESS_KEY = s3config.accessKey;
 const SECRET_ACCESS_KEY = s3config.secretAccessKey;
@@ -11,25 +12,20 @@ const myBucket = new AWS.S3({
   region: REGION,
 });
 
+// AWS ACCESS KEY를 세팅합니다.
+AWS.config.update({
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
+});
+
 export async function onFileUpload(file: File) {
-  // AWS ACCESS KEY를 세팅합니다.
-  AWS.config.update({
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_ACCESS_KEY,
-  });
-
-  // 버킷에 맞는 이름과 리전을 설정합니다.
-
-  // 파일과 파일 이름을 넘겨주면 됩니다.
-  const params = {
-    ACL: "public-read",
-    Body: file,
-    Bucket: S3_BUCKET,
-    Key: file.name,
-  };
-
   myBucket
-    .putObject(params)
+    .putObject({
+      ACL: "public-read",
+      Body: file,
+      Bucket: S3_BUCKET,
+      Key: file.name,
+    })
     .on("httpUploadProgress", (evt) => {
       alert("SUCCESS");
     })
@@ -38,12 +34,24 @@ export async function onFileUpload(file: File) {
     });
 }
 
-function readObject() {
-  var params = {
-    Bucket: S3_BUCKET, // replace with your bucket name
-  };
-  myBucket.listObjects(params, function (err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data); // successful response
-  });
+export async function readList() {
+  try {
+    const list = await myBucket.listObjects({ Bucket: S3_BUCKET }).promise();
+    return list;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+export async function readObject(key: string) {
+  try {
+    const data = await myBucket
+      .getObject({ Bucket: S3_BUCKET, Key: key })
+      .promise();
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
